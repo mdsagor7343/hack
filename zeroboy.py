ecbc8e51f45af061f865d2631098d2591e87b625
mdsagor7343 <66302730+mdsagor7343@users.noreply.github.com>
2020-06-26 03:52:08 +0600

    Update zeroboy.py

--- zeroboy.py
+++ zeroboy.py
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•THE - SAGOR -OFFICAL------•◈•
+|__\~~) •||•SAGOR - HACKERS---------------•◈•
+|__(-----\  •◈•------BLACK-HACKERS--------•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mSagor Hackers
+\033[1;96mInstagram \033[1;93m: \033[1;FlowSagor
+\033[1;96mFacebook  \033[1;93m: \033[1; Sagor7343
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Thesagor/Hacked
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,16 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92mSagor Hackers
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlowSagor
+
+\033[1;96mFacebook  \033[1;93m: \033[1;92m Sagor7343
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Thesagor/Hacked
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "sagor"
+CorrectPassword = "sagor"
 
 loop = 'true'
 while (loop == 'true'):
